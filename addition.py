7c7f232468f992ac28de35cd4983fd5b66dfd187
sand47 <sandeepkumaramni@gmail.com>
2021-01-13 19:54:15 -0800

    added mul fn

--- addition.py
+++ addition.py
@@ -8,7 +8,11 @@ def sub(a,b):
 def div(a,b):
     return a/b
 
-print(division)
+def mul(a,b):
+    return a*b
+
+print(mul(2,2))
+print(div(1,1))
 print(addition(1,2))
 print(addition(1.2,2.2))
 print(sub(1,2))
