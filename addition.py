e142a4533cf09c87ace26248c65dfbbf268e60d9
sand47 <sandeepkumaramni@gmail.com>
2021-01-13 19:58:06 -0800

    nothing

--- addition.py
+++ addition.py
@@ -5,6 +5,17 @@ def addition(a,b):
 def sub(a,b):
     return a-b
 
+def div(a,b):
+    return a/b
+
+def mul(a,b):
+    return a*b
+
+def nothing():
+    pass
+
+print(mul(2,2))
+print(div(1,1))
 print(addition(1,2))
 print(addition(1.2,2.2))
 print(sub(1,2))
